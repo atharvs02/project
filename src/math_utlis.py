5365b5323ae253bdaa7118137b90237e8414610e
atharvs02 <atharvsathe212@gmail.com>
2025-07-24 15:40:42 +0530

    Create math_utlis.py

---
+++ src/math_utlis.py
@@ -0,0 +1,10 @@
+def add(a,b):
+  """
+  Adds two numbers and returns the result.
+  Parameters:
+  a(int):frist number
+  b(int):Second number
+  Returns:
+  int:Sum of a and b
+  """
+  return a+b 
